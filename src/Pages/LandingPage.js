1c19f041233763ebc44a8fcf188d74fa49ca827b
alexfortin <fortin.alex14@gmail.com>
2024-01-08 22:11:22 -0600

    fixed the scrolling images in the landing page, cleaned some of the code, and made the landing page responsive, and also added more images so there is more variety in the image slider

--- src/Pages/LandingPage.js
+++ src/Pages/LandingPage.js
@@ -1,35 +1,43 @@
-import "./landingPage.css"
-import scaleUp from "../images/Frame 2.svg"
-import scaleDown from "../images/Frame 16.svg"
+import "./landingPage.css";
+import scaleUp from "../images/Frame 2.svg";
+import scaleDown from "../images/Frame 16.svg";
+import photographUp from "../images/photgraphUp.svg";
+import photographDown from "../images/photographDown.svg";
+import randomChords from "../images/Frame 19.svg";
+import random from "../images/Frame 20.svg";
 
 const LandingPage = () => {
   return (
     <div className="landing-page">
       <div className="landing-page-content">
-        <div className="max-width-wrapper">
+        <div className="content-container">
           <div className="text-container">
-            <h1>PIANO NOTE <br/> PRACTICE</h1>
+            <h1>
+              PIANO NOTE <br /> PRACTICE
+            </h1>
           </div>
-          <button>GET STARTED</button>
+          <button className="landing-page-btn">GET STARTED</button>
           <div className="image-slider-container">
-          <div className="image-slider">
-            <img className="landing-page-scale-img" alt="" src={scaleUp}/>
-            <img className="landing-page-scale-img" alt="" src={scaleDown}/>
-          </div>
-          <div className="image-slider">
-            <img className="landing-page-scale-img" alt="" src={scaleUp}/>
-            <img className="landing-page-scale-img" alt="" src={scaleDown}/>
-          </div>
-          <div className="image-slider">
-            <img className="landing-page-scale-img" alt="" src={scaleUp}/>
-            <img className="landing-page-scale-img" alt="" src={scaleDown}/>
-          </div>
+            <div className="image-slider">
+              <img className="landing-page-scale-img" alt="" src={scaleUp} />
+              <img className="landing-page-scale-img" alt="" src={scaleDown} />
+              <img className="landing-page-scale-img" alt="" src={randomChords} />
+              <img className="landing-page-scale-img" alt="" src={random} />
+              <img className="landing-page-scale-img" alt="" src={photographUp} />
+              <img className="landing-page-scale-img" alt="" src={photographDown} />
+              <img className="landing-page-scale-img" alt="" src={scaleUp} />
+              <img className="landing-page-scale-img" alt="" src={scaleDown} />
+              <img className="landing-page-scale-img" alt="" src={randomChords} />
+              <img className="landing-page-scale-img" alt="" src={random} />
+              <img className="landing-page-scale-img" alt="" src={photographUp} />
+              <img className="landing-page-scale-img" alt="" src={photographDown} />
+            </div>
           </div>
         </div>
+        <div className="bg-image-container"></div>
       </div>
-      <div className="bg-image-container"></div>
     </div>
   );
 };
 
-export default LandingPage
+export default LandingPage;
