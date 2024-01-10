09d884f74991ce4d6e65befb835b251cc3e2a1d6
alexfortin <fortin.alex14@gmail.com>
2024-01-09 23:45:45 -0600

    added react router and also changed the names of the images and fonts to be more clear

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import LandingPage from "./Pages/LandingPage";
 import PianoContainer from "./components/PianoContainer/PianoContainer";
-import "./style.css"
+import "./style.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <LandingPage />,
+  },
+  {
+    path: "piano",
+    element: <PianoContainer />,
+  },
+]);
 
 function App() {
   return (
     <div className="App">
-     {/* <PianoContainer/> */}
-     <LandingPage/>
+      <RouterProvider router={router} />
     </div>
   );
 }
--- src/Pages/LandingPage.js
+++ src/Pages/LandingPage.js
@@ -1,10 +1,12 @@
 import "./landingPage.css";
-import scaleUp from "../images/Frame 2.svg";
-import scaleDown from "../images/Frame 16.svg";
+import scaleUp from "../images/scaleUp.svg";
+import scaleDown from "../images/scaleDown.svg";
 import photographUp from "../images/photgraphUp.svg";
 import photographDown from "../images/photographDown.svg";
-import randomChords from "../images/Frame 19.svg";
-import random from "../images/Frame 20.svg";
+import randomChords from "../images/chords.svg";
+import random from "../images/randomProgression.svg";
+
+import {Link} from "react-router-dom"
 
 const LandingPage = () => {
   return (
@@ -16,7 +18,7 @@ const LandingPage = () => {
               PIANO NOTE <br /> PRACTICE
             </h1>
           </div>
-          <button className="landing-page-btn">GET STARTED</button>
+          <Link to={"/piano"} className="landing-page-btn">GET STARTED</Link>
           <div className="image-slider-container">
             <div className="image-slider">
               <img className="landing-page-scale-img" alt="" src={scaleUp} />
