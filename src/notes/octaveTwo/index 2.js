e17e93e393a6631a11c99f9cbea11cda9e4f3191
alexfortin <fortin.alex14@gmail.com>
2024-01-18 16:48:04 -0600

    merged with develop branch

---
+++ src/notes/octaveTwo/index 2.js
@@ -0,0 +1,31 @@
+import noteC from "./2-c.wav";
+import noteCs from "./2-cs.wav";
+import noteD from "./2-d.wav";
+import noteDs from "./2-ds.wav";
+import noteE from "./2-e.wav";
+import noteF from "./2-f.wav";
+import noteFs from "./2-fs.wav";
+import noteG from "./2-g.wav";
+import noteGs from "./2-gs.wav";
+import noteA from "./2-a.wav";
+import noteAs from "./2-as.wav";
+import noteB from "./2-b.wav";
+import octaveC from "../octaveThree/3-c.wav";
+
+const octaveTwoNotes = {
+  noteC,
+  noteCs,
+  noteD,
+  noteDs,
+  noteE,
+  noteF,
+  noteFs,
+  noteG,
+  noteGs,
+  noteA,
+  noteAs,
+  noteB,
+  octaveC,
+};
+
+export default octaveTwoNotes;
