83b0b9f0591a64956ace77b22364898b95133ffd
alexfortin <fortin.alex14@gmail.com>
2024-01-23 16:58:54 -0600

    styled the piano controller, changed the way the user will select which octave they want to play, and also all the octave data will be stored inside an array

--- src/components/PianoContainer/PianoContainer.js
+++ src/components/PianoContainer/PianoContainer.js
@@ -3,31 +3,33 @@ import Piano from "../Piano/Piano";
 import PianoController from "./PianoController";
 
 import "./pianoContainer.css";
-import getOctaveNotes from "../Piano/octaveNotes";
+import octaveNotes from "../Piano/octaveNotes";
 
 const PianoContainer = () => {
   const [noteDisplay, setNoteDisplay] = useState("notes");
-  const [currentClefNotes, setCurrentClefNotes] = useState(getOctaveNotes(2));
+  const [currentClefNotes, setCurrentClefNotes] = useState(octaveNotes[2]);
   const [pianoHasFocus, setPianoHasFocus] = useState(true);
   const [isSustainOn, setIsSustainOn] = useState(false);
 
   return (
     <div className="piano-container">
-      <PianoController
-        noteDisplay={noteDisplay}
-        setNoteDisplay={setNoteDisplay}
-        setCurrentClefNotes={setCurrentClefNotes}
-        setPianoHasFocus={setPianoHasFocus}
-        isSustainOn={isSustainOn}
-        setIsSustainOn={setIsSustainOn}
-      />
-      <Piano
-        noteDisplay={noteDisplay}
-        clefNotes={currentClefNotes}
-        setPianoHasFocus={setPianoHasFocus}
-        pianoHasFocus={pianoHasFocus}
-        isSustainOn={isSustainOn}
-      />
+      <div className="max-width-wrapper">
+        <PianoController
+          noteDisplay={noteDisplay}
+          setNoteDisplay={setNoteDisplay}
+          setCurrentClefNotes={setCurrentClefNotes}
+          setPianoHasFocus={setPianoHasFocus}
+          isSustainOn={isSustainOn}
+          setIsSustainOn={setIsSustainOn}
+        />
+        <Piano
+          noteDisplay={noteDisplay}
+          clefNotes={currentClefNotes}
+          setPianoHasFocus={setPianoHasFocus}
+          pianoHasFocus={pianoHasFocus}
+          isSustainOn={isSustainOn}
+        />
+      </div>
     </div>
   );
 };
