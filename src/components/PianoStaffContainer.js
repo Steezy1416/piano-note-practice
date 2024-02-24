ac5b0805eb574909c84df551c1be1151c3796922
alexfortin <fortin.alex14@gmail.com>
2024-02-24 02:52:45 -0600

    the questions are now randomized and never ending, also adjusted the sizing on elements so its more adaptable to different screen sizes

--- src/components/PianoStaffContainer.js
+++ src/components/PianoStaffContainer.js
@@ -1,20 +1,28 @@
-import { useState } from "react"
-import MusicStaff from "./musicStaff/MusicStaff"
-import PianoContainer from "./PianoContainer/PianoContainer"
-import { bassClefQuestions, trebleClefQuestions } from "./musicStaff/noteQuestions"
+import { useState } from "react";
+import MusicStaff from "./musicStaff/MusicStaff";
+import PianoContainer from "./PianoContainer/PianoContainer";
+import {
+  bassClefQuestions,
+  trebleClefQuestions,
+} from "./musicStaff/noteQuestions";
 
 const PianoStaffContainer = () => {
+  const [currentQuestionIndex, setCurrentQuestionIndex] = useState(Math.floor(Math.random() * 23));
+  const [currentQuestions, setCurrentQuestions] = useState(trebleClefQuestions);
 
-    const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0)
-    const [currentQuestions, setCurrentQuestions] = useState(trebleClefQuestions)
+  return (
+    <div className="app-container">
+      <MusicStaff
+        currentQuestionIndex={currentQuestionIndex}
+        currentQuestions={currentQuestions}
+      />
+      <PianoContainer
+        currentQuestions={currentQuestions}
+        currentQuestionIndex={currentQuestionIndex}
+        setCurrentQuestionIndex={setCurrentQuestionIndex}
+      />
+    </div>
+  );
+};
 
-
-    return (
-        <div className="app-container">
-            <MusicStaff currentQuestionIndex={currentQuestionIndex} currentQuestions={currentQuestions}/>
-            <PianoContainer currentQuestions={currentQuestions} currentQuestionIndex={currentQuestionIndex} setCurrentQuestionIndex={setCurrentQuestionIndex}/>
-        </div>
-    )
-}
-
-export default PianoStaffContainer
+export default PianoStaffContainer;
